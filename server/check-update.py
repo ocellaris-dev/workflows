e1208fd05f1aeac62a50100f7668c48f79ffaf3a
Mincart8925 <minecart8926@daum.net>
2022-07-19 18:05:43 +0900

    remove old scripts

--- server/check-update.py
+++
@@ -1,9 +0,0 @@
-import utils
-import github
-
-if not utils.get_package_info() == get_misskey_info():
-    print("New version detected.")
-    github.trigger_actions()
-else:
-    print("Misskey is up-to-date!")
-    exit()
--- server/github.py
+++
@@ -1,5 +0,0 @@
-import requests
-import os
-
-def trigger_actions():
-   # res = requests.post("https://api.github.com/repos/ocellaris-dev/workflows/dispatches")
