93fef212f5d2ce417e8a0b9b0c32840205beaff1
caipira113 <caipira113@ocellaris.dev>
2022-07-19 20:01:33 +0900

    chore: Remove 1 line

--- server/version-check.sh
+++ server/version-check.sh
@@ -18,7 +18,6 @@ if [ "$ins_ver" = "$repo_ver" ]; then
     echo "-> Misskey is up-to-date!"
     bash /home/caipira113/cron.sh &
     gh workflow run merge-upstream.yml -f docker=true -R ocellaris-dev/stella
-    sudo service cron start
     exit 0
 else
     echo "-> New version detected."
