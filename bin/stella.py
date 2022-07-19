aa4ac4cd0153dfde28c05f227345f8af5a156713
Mincart8925 <minecart8926@daum.net>
2022-07-19 18:28:26 +0900

    fix snapshot name is not correct

--- bin/stella.py
+++ bin/stella.py
@@ -5,7 +5,7 @@ import time
 def build_snapshot():
     utc_str = utils.get_snapshot_utc_time()
     random_str = utils.gen_str()
-    snapshot_name = "stella" + "-" + utc_str + "-" + random_str
+    snapshot_name = "stella-asia-northeast2-b" + "-" + utc_str + "-" + random_str
     subprocess.call(["gcloud", "compute", "snapshots", "create", snapshot_name, '--source-disk', 'stella', '--source-disk-zone=asia-northeast2-b', "--storage-location=asia-northeast2", "-q"])
     return snapshot_name
 
