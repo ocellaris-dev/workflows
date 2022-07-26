ca3ae6abb99ecb407b3b3dd876e00c12c5e27626
Mincart8925 <minecart8926@daum.net>
2022-07-26 14:23:04 +0900

    more f-string formatting

--- stella/utils.py
+++ stella/utils.py
@@ -24,7 +24,8 @@ def gen_str(size=8):
 
 def get_time():
     tm = time.localtime(time.time())
-    return time.strftime("%H:%M", tm) + " (UTC+9)"
+    stt = time.strftime("%H:%M", tm)
+    return f "{stt} (UTC+9)"
 
 
 def get_snapshot_utc_time():
